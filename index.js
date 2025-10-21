8f60e60cc85ae824362e7bb958323a1ee753d37b
aishwaryacscs <caishwaryacs@gmail.com>
2025-10-21 09:38:23 +0530

    Implement number guessing game endpoint
    Added a guessing game feature to the app.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send("Hello from Guess Number game!"));
+// Random number between 1 and 100
+let numberToGuess = Math.floor(Math.random() * 100) + 1;
+
+app.get('/guess/:num', (req, res) => {
+    const userGuess = parseInt(req.params.num);
+    if(userGuess > numberToGuess) res.send('Too high!');
+    else if(userGuess < numberToGuess) res.send('Too low!');
+    else res.send('Correct! You guessed it!');
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
